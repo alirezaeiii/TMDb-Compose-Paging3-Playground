246f9137d2d23a25b318e48e5d9b6c261522f694
Ali <m.ali.rezaeiii09@gmail.com>
2025-10-11 01:11:48 +0330

    Simplify feed screen

--- features/feature-feed/src/main/java/com/sample/tmdb/feed/FeedScreen.kt
+++ features/feature-feed/src/main/java/com/sample/tmdb/feed/FeedScreen.kt
@@ -44,6 +44,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import com.sample.tmdb.common.MainDestinations
+import com.sample.tmdb.common.R as commonR
 import com.sample.tmdb.common.model.TMDbItem
 import com.sample.tmdb.common.ui.Content
 import com.sample.tmdb.common.ui.Dimens
@@ -64,7 +65,6 @@ import com.sample.tmdb.domain.model.Movie
 import com.sample.tmdb.domain.model.SortType
 import com.sample.tmdb.domain.model.TVShow
 import com.sample.tmdb.feed.utils.pagerTransition
-import com.sample.tmdb.common.R as commonR
 
 @Composable
 fun MovieFeedScreen(navController: NavController, viewModel: MovieFeedViewModel = hiltViewModel()) {
