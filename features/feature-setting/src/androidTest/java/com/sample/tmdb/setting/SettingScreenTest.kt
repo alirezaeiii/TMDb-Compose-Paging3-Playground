d2ecc006a032cb7aeb466aa300ae569dd08ea635
Ali <m.ali.rezaeiii09@gmail.com>
2026-02-22 17:00:20 +0330

    cleanup

--- features/feature-setting/src/androidTest/java/com/sample/tmdb/setting/SettingScreenTest.kt
+++ features/feature-setting/src/androidTest/java/com/sample/tmdb/setting/SettingScreenTest.kt
@@ -12,39 +12,6 @@ class SettingScreenTest {
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
     @Test
-    fun settingScreenTest() {
-        with(composeTestRule) {
-            setContent {
-                SettingsScreen()
-            }
-            onNodeWithText(activity.getString(R.string.language)).assertIsDisplayed()
-            onNodeWithText(activity.getString(R.string.source_code_github)).assertIsDisplayed()
-            onNodeWithText(activity.getString(R.string.privacy_policy)).assertIsDisplayed()
-            onNodeWithText(activity.getString(R.string.version)).assertIsDisplayed()
-            onNodeWithText(activity.getString(R.string.settings)).assertIsDisplayed()
-        }
-    }
-
-    @Test
-    fun settingsGroupItemTest() {
-        with(composeTestRule) {
-            setContent {
-                SettingsGroupItem(
-                    listOf(
-                        Settings.Info(
-                            R.drawable.ic_info,
-                            R.string.version,
-                            "1.1",
-                        ),
-                    ),
-                )
-            }
-            onNodeWithText(activity.getString(R.string.version)).assertIsDisplayed()
-            onNodeWithText("1.1").assertIsDisplayed()
-        }
-    }
-
-    @Test
     fun titleText() {
         with(composeTestRule) {
             setContent {
